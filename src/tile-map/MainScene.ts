eba698c08357251beba45404126f034b3b3351f3
Pythongor <djmozartd@gmail.com>
2026-02-17 11:37:46 +0200

    Splitted Phaser logic and presentation
    Splitted Phaser model and view/controller

--- src/tile-map/MainScene.ts
+++ src/tile-map/MainScene.ts
@@ -2,29 +2,17 @@ import Phaser from "phaser";
 import { EventBus } from "./EventBus";
 import { LayerType } from "@store/brushSlice";
 import { type BrushSettings } from "@store/selectors";
-import { identifyBiome, getBiomeColor } from "@/helpers/biomeResolver";
-import { getLayerColor } from "@/helpers/paletteResolver";
+import { worldManager } from "@tile-map/WorldManager";
+import { getBiomeColor } from "@helpers/biomeResolver";
+import { getLayerColor } from "@helpers/paletteResolver";
 
 export class MainScene extends Phaser.Scene {
-  private gridSize: number = 129;
   private tileSize: number = 16;
-
   private currentLayer: LayerType = LayerType.Elevation;
   private viewMode: LayerType | "biomes" = "biomes";
   private brushValue: number = 100;
   private brushSize: number = 1;
-
-  private worldData: Record<LayerType, Uint16Array> = {
-    elevation: new Uint16Array(this.gridSize * this.gridSize).fill(100),
-    rainfall: new Uint16Array(this.gridSize * this.gridSize).fill(50),
-    drainage: new Uint16Array(this.gridSize * this.gridSize).fill(50),
-    temperature: new Uint16Array(this.gridSize * this.gridSize).fill(50),
-    volcanism: new Uint16Array(this.gridSize * this.gridSize).fill(0),
-    savagery: new Uint16Array(this.gridSize * this.gridSize).fill(0),
-  };
-
   private displayGraphics?: Phaser.GameObjects.Graphics;
-
   private isPanning: boolean = false;
 
   constructor() {
@@ -33,9 +21,9 @@ export class MainScene extends Phaser.Scene {
 
   create() {
     this.displayGraphics = this.add.graphics();
+    const worldWidth = worldManager.gridSize * this.tileSize;
+    const worldHeight = worldManager.gridSize * this.tileSize;
 
-    const worldWidth = this.gridSize * this.tileSize;
-    const worldHeight = this.gridSize * this.tileSize;
     this.cameras.main.setBounds(0, 0, worldWidth, worldHeight);
 
     EventBus.on("brush-updated", (state: BrushSettings) => {
@@ -48,51 +36,36 @@ export class MainScene extends Phaser.Scene {
       }
     });
 
+    this.setupInputs();
     this.redrawMap();
+    this.cameras.main.centerOn(worldWidth / 2, worldHeight / 2);
+  }
 
-    this.input.on("pointerdown", (pointer: Phaser.Input.Pointer) => {
-      if (pointer.middleButtonDown()) {
-        this.isPanning = true;
-      } else {
-        this.handleInput(pointer);
-      }
+  private setupInputs() {
+    this.input.on("pointerdown", (p: Phaser.Input.Pointer) => {
+      if (p.middleButtonDown()) this.isPanning = true;
+      else this.handleInput(p);
     });
 
-    this.input.on("pointerup", () => {
-      this.isPanning = false;
-    });
+    this.input.on("pointerup", () => (this.isPanning = false));
 
-    this.input.on("pointermove", (pointer: Phaser.Input.Pointer) => {
+    this.input.on("pointermove", (p: Phaser.Input.Pointer) => {
       if (this.isPanning) {
         this.cameras.main.scrollX -=
-          (pointer.x - pointer.prevPosition.x) / this.cameras.main.zoom;
+          (p.x - p.prevPosition.x) / this.cameras.main.zoom;
         this.cameras.main.scrollY -=
-          (pointer.y - pointer.prevPosition.y) / this.cameras.main.zoom;
-      } else if (pointer.isDown) {
-        this.handleInput(pointer);
+          (p.y - p.prevPosition.y) / this.cameras.main.zoom;
+      } else if (p.isDown) {
+        this.handleInput(p);
       }
 
-      const tileX = Math.floor(pointer.worldX / this.tileSize);
-      const tileY = Math.floor(pointer.worldY / this.tileSize);
+      const tx = Math.floor(p.worldX / this.tileSize);
+      const ty = Math.floor(p.worldY / this.tileSize);
 
-      EventBus.emit("update-coords", {
-        x: tileX,
-        y: tileY,
-      });
-
-      if (
-        tileX >= 0 &&
-        tileX < this.gridSize &&
-        tileY >= 0 &&
-        tileY < this.gridSize
-      ) {
-        const index = tileY * this.gridSize + tileX;
-
-        const pointData = this.getPointData(index);
-
-        const biome = identifyBiome(pointData);
-
-        EventBus.emit("update-biome", biome);
+      if (this.isValidTile(tx, ty)) {
+        const index = ty * worldManager.gridSize + tx;
+        EventBus.emit("update-coords", { x: tx, y: ty });
+        EventBus.emit("update-biome", worldManager.getBiome(index));
       }
     });
 
@@ -100,57 +73,41 @@ export class MainScene extends Phaser.Scene {
       "wheel",
       (
         _: Phaser.Input.Pointer,
-        __: Phaser.GameObjects.GameObject[],
+        __: Phaser.GameObjects.GameObject,
         ___: number,
         deltaY: number,
       ) => {
-        const zoomSpeed = 0.001;
-        const newZoom = this.cameras.main.zoom - deltaY * zoomSpeed;
+        const newZoom = this.cameras.main.zoom - deltaY * 0.001;
         this.cameras.main.setZoom(Phaser.Math.Clamp(newZoom, 0.2, 3));
       },
     );
-
-    this.redrawMap();
-    this.cameras.main.centerOn(worldWidth / 2, worldHeight / 2);
-  }
-
-  getPointData(index: number) {
-    return {
-      elevation: this.worldData.elevation[index],
-      rainfall: this.worldData.rainfall[index],
-      drainage: this.worldData.drainage[index],
-      temperature: this.worldData.temperature[index],
-      volcanism: this.worldData.volcanism[index],
-    };
   }
 
   getTileColor(index: number): number {
-    const pointData = this.getPointData(index);
+    const data = worldManager.getPointData(index);
 
     if (this.viewMode === "biomes") {
-      const biomeType = identifyBiome(pointData);
-      const isVolcanic = pointData.volcanism > 90;
-
-      return getBiomeColor(biomeType, isVolcanic);
+      return getBiomeColor(worldManager.getBiome(index), data.volcanism > 90);
     }
-
-    return getLayerColor(this.viewMode, this.worldData[this.viewMode][index]);
+    return getLayerColor(
+      this.viewMode,
+      worldManager.worldData[this.viewMode][index],
+    );
   }
 
   paintTile(tileX: number, tileY: number) {
     if (!this.displayGraphics) return;
+    const half = Math.floor(this.brushSize / 2);
 
-    const halfSize = Math.floor(this.brushSize / 2);
-
-    for (let dy = -halfSize; dy <= halfSize; dy++) {
-      for (let dx = -halfSize; dx <= halfSize; dx++) {
+    for (let dy = -half; dy <= half; dy++) {
+      for (let dx = -half; dx <= half; dx++) {
         const tx = tileX + dx;
         const ty = tileY + dy;
 
-        if (tx >= 0 && tx < this.gridSize && ty >= 0 && ty < this.gridSize) {
-          const index = ty * this.gridSize + tx;
+        if (this.isValidTile(tx, ty)) {
+          const index = ty * worldManager.gridSize + tx;
 
-          this.worldData[this.currentLayer][index] = this.brushValue;
+          worldManager.updateTile(index, this.currentLayer, this.brushValue);
 
           const color = this.getTileColor(index);
           this.displayGraphics.fillStyle(color, 1);
@@ -167,28 +124,34 @@ export class MainScene extends Phaser.Scene {
 
   redrawMap() {
     if (!this.displayGraphics) return;
-
     this.displayGraphics.clear();
-    for (let y = 0; y < this.gridSize; y++) {
-      for (let x = 0; x < this.gridSize; x++) {
-        const index = y * this.gridSize + x;
-        const color = this.getTileColor(index);
-        this.displayGraphics.fillStyle(color, 1);
-        this.displayGraphics.fillRect(x * 16, y * 16, 16, 16);
-      }
+    const size = worldManager.gridSize;
+
+    for (let i = 0; i < size * size; i++) {
+      const x = i % size;
+      const y = Math.floor(i / size);
+      const color = this.getTileColor(i);
+      this.displayGraphics.fillStyle(color, 1);
+      this.displayGraphics.fillRect(
+        x * this.tileSize,
+        y * this.tileSize,
+        this.tileSize,
+        this.tileSize,
+      );
     }
+  }
+
+  private isValidTile(x: number, y: number) {
+    return (
+      x >= 0 && x < worldManager.gridSize && y >= 0 && y < worldManager.gridSize
+    );
   }
 
   private handleInput(pointer: Phaser.Input.Pointer) {
     const tileX = Math.floor(pointer.worldX / this.tileSize);
     const tileY = Math.floor(pointer.worldY / this.tileSize);
 
-    if (
-      tileX >= 0 &&
-      tileX < this.gridSize &&
-      tileY >= 0 &&
-      tileY < this.gridSize
-    ) {
+    if (this.isValidTile(tileX, tileY)) {
       this.paintTile(tileX, tileY);
     }
   }
---
+++ src/tile-map/WorldManager.ts
@@ -0,0 +1,39 @@
+import { LayerType } from "@store/brushSlice";
+import { identifyBiome } from "@/helpers/biomeResolver";
+
+export class WorldManager {
+  public gridSize: number = 129;
+  public worldData: Record<LayerType, Uint16Array>;
+
+  constructor() {
+    this.worldData = {
+      elevation: new Uint16Array(this.gridSize * this.gridSize).fill(100),
+      rainfall: new Uint16Array(this.gridSize * this.gridSize).fill(50),
+      drainage: new Uint16Array(this.gridSize * this.gridSize).fill(50),
+      temperature: new Uint16Array(this.gridSize * this.gridSize).fill(50),
+      volcanism: new Uint16Array(this.gridSize * this.gridSize).fill(0),
+      savagery: new Uint16Array(this.gridSize * this.gridSize).fill(0),
+    };
+  }
+
+  getPointData(index: number) {
+    return {
+      elevation: this.worldData.elevation[index],
+      rainfall: this.worldData.rainfall[index],
+      drainage: this.worldData.drainage[index],
+      temperature: this.worldData.temperature[index],
+      volcanism: this.worldData.volcanism[index],
+      savagery: this.worldData.savagery[index],
+    };
+  }
+
+  updateTile(index: number, layer: LayerType, value: number) {
+    this.worldData[layer][index] = value;
+  }
+
+  getBiome(index: number) {
+    return identifyBiome(this.getPointData(index));
+  }
+}
+
+export const worldManager = new WorldManager();
